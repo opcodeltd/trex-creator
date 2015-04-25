82122a896069c9a8b3d5620831df0bc9f09ce2dc
Nigel McNie <nigel@mcnie.name>
2015-04-25 12:31:31 +1200

    pedantic whitespace

--- templates/app/model.py
+++ templates/app/model.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 from __future__ import absolute_import
+
 from app import app
 from mongoengine import *
 from trex.support.model import BaseDocument as Document, BaseAudit, BaseIdentity, BaseUser
